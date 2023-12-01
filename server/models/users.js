22fea0898060ee77304a63d6ccce5e3ac34fc983
DPK-7 <prabhudpk789@gmail.com>
2023-11-30 23:09:30 -0500

    Backend setup

---
+++ server/models/users.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        "userName": "Prabhdpk7",
+        "password": "dpk*789"
+    }, {
+        "userName": "dpk789",
+        "password": "dpk@7890"
+    }, {
+        "userName": "Prabhudpk789",
+        "password": "dpk*7"
+    },
+]
+
+exports.getAllUsers = () =>{
+    return users;
+}
+
